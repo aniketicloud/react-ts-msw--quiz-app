0d0c25112517f3bb09e60a8f583a52f465d624fc
Aniket Pandharabale <aniketicloud@gmail.com>
2023-10-13 16:01:25 +0530

    mocked server worker for questions

---
+++ src/constants.ts
@@ -0,0 +1,1 @@
+export const questionsApi = "https://someserver.com/questions";
---
+++ src/mocks/handlers.ts
@@ -0,0 +1,10 @@
+// src/mocks/handlers.js
+import { rest } from "msw";
+import { questionsApi } from "../constants";
+import { questions } from "./questions.json";
+
+export const handlers = [
+  rest.get(questionsApi, (req, res, ctx) => {
+    return res(ctx.status(200), ctx.json(questions));
+  }),
+];
